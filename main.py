b35706e5e41273aaa716ffca4a72a35e02554431
Amr Mohamed <amro.muh2@gmail.com>
2023-08-09 11:48:52 +0100

    New project

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return '<h1>Home Page</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>About page</h1>'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
